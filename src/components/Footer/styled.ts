4c71519b93435480f513bed48d5ed87783df3831
Daer0n <danilatarbaya57@gmail.com>
2024-11-15 01:41:51 +0300

    feat: add Footer

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { COLORS } from '@constants';
+
+export const Container = styled.div`
+    height: 160px;
+    background-color: ${COLORS.blueDark};
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+`;
